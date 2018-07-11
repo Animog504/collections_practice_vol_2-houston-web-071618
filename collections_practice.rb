8ea4ea7e55eb2da4b4eae97f2c33247ae8e6e4c9
Scott Ungchusri <animoogle504@gmail.com>
2018-07-11 03:59:46 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
 # your code goes here
+def begins_with_r(array)
+  bool = true
+  array.each do |x|
+    if (x.start_with?("r") && bool)
+      bool = true
+    else
+      bool = false
+      break
+    end
+  end 
+  bool
+end
+
+def contain_a(array)
+  iterator = 0
+  returnArray = []
+  while iterator < array.length
+    array.each do |x|
+      if x.include?("a")
+        returnArray << array[iterator]
+      end
+      iterator+=1
+    end 
+  end
+  returnArray
+end
+
+def first_wa(array)
+  found = false
+  while !found do |x|
+   if x.include?("wa")
+     
+end
+
+def remove_non_strings
+  
+end
+
+def count_elements
+  
+end
+
+def merge_data
+  
+end
+
+def find_cool
+  
+end
+
+def organized_schools
+  
+end
